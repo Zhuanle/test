4ec9fcdaf2d406a32629ca835bcdf924fa3cbdb1
Zhuanle <1847496393@qq.com>
2019-12-16 13:56:37 +0800

    test2

--- src/main/java/zhl/App.java
+++ src/main/java/zhl/App.java
@@ -9,9 +9,9 @@ public class App
     public static void main( String[] args )
     {
         try {
+            System.out.println("注册驱动");
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("初始化成功");
-            System.out.println("更新");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
