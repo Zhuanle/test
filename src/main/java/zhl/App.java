fe2776aeb3720550a842f5cc66426bbade336809
Zhuanle <1847496393@qq.com>
2019-12-16 13:30:48 +0800

    test2

--- src/main/java/zhl/App.java
+++ src/main/java/zhl/App.java
@@ -11,6 +11,7 @@ public class App
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("初始化成功");
+            System.out.println("更新");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
